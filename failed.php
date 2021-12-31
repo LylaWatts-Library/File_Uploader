a615fb787e9ced395f2393dceb19d97c3459711e
Afnan <72243953+afnan007a@users.noreply.github.com>
2021-12-31 20:48:19 +0530

    Update failed.php

--- failed.php
+++ failed.php
@@ -30,6 +30,14 @@
     <br/>
     <br/>
     <h1><a href="./files/index.php"><strong><code>View your files!</code></strong></a></h1>
+      <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+    <h3 text-align="center"><a href="https://afnanksalal.xyz"><strong><code> <center>Made by Afnan</center></code></strong></a></h3>
 </body>
 
 </html>
